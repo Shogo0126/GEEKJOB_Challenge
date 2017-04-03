247b1c8c689ec0b0e0b751bc28d07493390ec10f
米坂　省吾 <yonesaka0126@gmail.com>
2017-04-03 15:12:55 +0900

    基礎学習１ー自己紹介の課題を提出します。

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/subject3.php
@@ -0,0 +1,18 @@
+<?php
+$message = "こんにちは。";//挨拶
+$name = "私の名前は米坂省吾です。";//名前
+$age = "年齢は23歳です。";//年齢
+$from = "出身地は北海道出身です。";//出身地
+$hobby = "趣味は筋トレです。";//趣味
+
+echo $message . "\n";
+echo "<br>";
+echo $name . "\n";
+echo "<br>";
+echo $age . "\n";
+echo "<br>";
+echo $from."\n";
+echo "<br>";
+echo $hobby."\n";
+
+ ?>
