159cc4ac517931e70489dacf28e7a19e950ac3ee
米坂　省吾 <yonesaka0126@gmail.com>
2017-04-03 15:13:32 +0900

    基礎学習１ー四則演算の課題を提出します。

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/subject4.php
@@ -0,0 +1,30 @@
+<?php
+$tasi = 12 + 15;
+$hiki = 78 - 35;
+$kake = 89 * 12;
+$wari = 45 / 19;
+$amari = 89 % 23;
+
+echo $tasi."\n";
+echo "<br>";
+echo $hiki."\n";
+echo "<br>";
+echo $kake."\n";
+echo "<br>";
+echo $wari."\n";
+echo "<br>";
+echo $amari."\n";
+echo "<br>";
+
+$a = 5;
+echo ++$a - 6;
+echo "<br>";
+$b = 9;
+echo --$b;
+echo "<br>";
+$c = 2;
+$c++;
+
+echo $c - 2;
+
+ ?>
