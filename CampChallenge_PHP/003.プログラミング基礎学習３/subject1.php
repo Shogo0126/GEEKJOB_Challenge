fbad0042caafa5f2a98cae35e9693d5a355c25aa
米坂　省吾 <yonesaka0126@gmail.com>
2017-04-06 05:34:30 +0900

    基礎課題３ーユーザ定義関数の課題を提出します。

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/subject1.php
@@ -0,0 +1,19 @@
+<?php
+function my_profile(){
+  echo "私の名前は米坂省吾です。<br>";
+  echo "1994年1月26日生まれです。<br>";
+  echo "趣味は筋トレです。<br>";
+}
+
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+my_profile();
+
+ ?>
